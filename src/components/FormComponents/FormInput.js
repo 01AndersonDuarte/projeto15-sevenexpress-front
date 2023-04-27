d7ac9067d702419b87dd1c56042f80ddd87081eb
Anderson Duarte <andersonduarte997@gmail.com>
2023-04-27 16:15:57 -0300

    refactor: ajustes no layout dos forms e nas mensagens de erros

--- src/components/FormComponents/FormInput.js
+++ src/components/FormComponents/FormInput.js
@@ -3,11 +3,15 @@ import styled from "styled-components"
 export const FormInput = styled.input`
     width: 60%;
     height: 45px;
+
+    border: solid 1px rgba(212, 212, 212, 1);
     border-radius: 5px;
+    outline: 0;
 
     box-sizing: border-box;
     padding: 10px;
     font-size: 18px;
+    font-weight: 400;
     @media screen and (max-width: 700px) {
         width: 100%;
     }
--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,31 +1,33 @@
-import { PageContainer, FormButton, FormInput, PageForm, StyledLink} from "../../components/FormComponents"
+import { PageContainer, FormButton, FormInput, PageForm, StyledLink } from "../../components/FormComponents"
 import axios from "axios"
 import { useState, useEffect } from "react"
 import useAuth from "../../hooks/useAuth"
 import { useNavigate } from "react-router-dom"
 import { port } from "../../port"
 
-export default function SingIn(){
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
+export default function SingIn() {
+    const [loginData, setLoginData] = useState({ email: "", password: "" })
+    // const [email, setEmail] = useState("")
+    // const [password, setPassword] = useState("")
 
-    const {auth, login} = useAuth()
+    const { auth, login } = useAuth()
     const navigate = useNavigate()
 
     console.log(auth)
 
     useEffect(() => {
-        if(auth){
+        if (auth) {
             navigate("/home")
         }
     }, [])
 
-    function singIn(e){
+    function singIn(e) {
         e.preventDefault()
 
         const url = `${port}/`
-        const body = {email, password}
-        axios.post(url, body)
+        // const body = { email, password }
+
+        axios.post(url, loginData)
             .then(sucess => {
                 login(sucess.data)
                 navigate("/home")
@@ -33,11 +35,34 @@ export default function SingIn(){
             .catch(fail => alert(fail.response.data))
     }
 
-    return(
+    function insertLoginData(event) {
+        event.target.setCustomValidity('');
+        const value = event.target.value;
+        const attribute = event.target.name;
+
+        setLoginData({ ...loginData, [attribute]: value });
+    }
+    // Função para ajudar na formatação do objeto que vai pra requisição de login
+    
+    return (
         <PageContainer>
             <PageForm onSubmit={singIn}>
-                <FormInput placeholder="E-Mail" type="email" required onChange={(e) => setEmail(e.target.value)}></FormInput>
-                <FormInput placeholder="Senha" type="password" required onChange={(e) => setPassword(e.target.value)}></FormInput>
+                <FormInput
+                    placeholder="E-mail"
+                    type="email"
+                    name="email"
+                    required
+                    onChange={insertLoginData}
+                    onInvalid={(event) => event.target.setCustomValidity('Por favor, insira um e-mail válido.')}
+                />
+                <FormInput
+                    placeholder="Senha"
+                    type="password"
+                    name="password"
+                    required
+                    onChange={insertLoginData}
+                    onInvalid={(event) => event.target.setCustomValidity('Insira sua senha.')}
+                />
                 <FormButton type="submit">Login</FormButton>
             </PageForm>
             <StyledLink to={"/cadastro"}>Nao possui uma conta? Faca o cadastro aqui!</StyledLink>
