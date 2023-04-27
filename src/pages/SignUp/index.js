33ffa652fdfd17f9fc49a4e48a5751caa87e5877
winiciuskyutoku <winicius123098@gmail.com>
2023-04-27 11:42:30 -0300

    feat: Submit do formulatio de cadastro

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,15 +1,34 @@
 import styled from "styled-components"
 import { Link } from "react-router-dom"
+import { port } from "../../port"
+import { useState } from "react"
+import axios from "axios"
 
 export default function SignUp() {
 
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [confirmPassword, setConfirmPassword] = useState("")
+
+    function signUp(e){
+        e.preventDefault()
+
+        if(password !== confirmPassword) return alert("As senhas nao sao compativeis")
+
+        const url = `${port}/cadastro`
+        const body = {name, email, password}
+
+        axios.post(url, body).then(sucess => alert(sucess.data)).catch(fail => alert(fail.response.data))
+    }
+
     return (
         <PageContainer>
-                <SignupForm>
-                    <FormInput placeholder="Nome"></FormInput>
-                    <FormInput placeholder="E-mail"></FormInput>
-                    <FormInput placeholder="Senha"></FormInput>
-                    <FormInput placeholder="Confirme a senha"></FormInput>
+                <SignupForm onSubmit={signUp}>
+                    <FormInput placeholder="Nome" type="text" required onChange={(e) => setName(e.target.value)}></FormInput>
+                    <FormInput placeholder="E-mail" type="email" required onChange={(e) => setEmail(e.target.value)}></FormInput>
+                    <FormInput placeholder="Senha" type="password" required onChange={(e) => setPassword(e.target.value)}></FormInput>
+                    <FormInput placeholder="Confirme a senha" type="password" required onChange={(e) => setConfirmPassword(e.target.value)}></FormInput>
                     <FormButton type="submit">Cadastro</FormButton>
                 </SignupForm>
                 <StyledLink>Ja tem cadastro? Entao siga para o seu login aqui!</StyledLink>
@@ -33,9 +52,9 @@ const SignupForm = styled.form`
     flex-direction: column;
     align-items: center;
 
-    width: 50%;
+    width: 700px;
     gap: 15px;
-    @media screen and (max-width: 900px){
+    @media screen and (max-width: 700px){
         width: 90%;
     }
 `
@@ -48,7 +67,7 @@ const FormInput = styled.input`
     box-sizing: border-box;
     padding: 10px;
     font-size: 18px;
-    @media screen and (max-width: 900px) {
+    @media screen and (max-width: 700px) {
         width: 100%;
     }
 `
@@ -63,7 +82,7 @@ const FormButton = styled.button`
     background-color: orange;
     color: #FFFFFF;
     font-size: 18px;
-    @media screen and (max-width: 900px) {
+    @media screen and (max-width: 700px) {
         width: 100%;
     }
 `
@@ -74,7 +93,7 @@ const StyledLink = styled(Link)`
     margin-top: 15px;
     width: 60%;
     text-align: center;
-    @media screen and (max-width: 900px){
+    @media screen and (max-width: 700px){
         width: 90%;
     }
 `
