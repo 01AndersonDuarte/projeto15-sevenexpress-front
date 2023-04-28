c1cf2fdb172257d28a25b1bab34aa290685926ae
Anderson Duarte <andersonduarte997@gmail.com>
2023-04-27 21:38:33 -0300

    refactor: mudanças no signup

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -4,26 +4,32 @@ import { useState } from "react"
 import axios from "axios"
 import { FormContainer, FormImage, PageContainer, FormButton, FormInput, PageForm, StyledLink } from "../../components/FormComponents"
 import logo from "../../assets/Orange_Simple_Online_Shopping_Logo_1.png"
-
+import { useNavigate } from "react-router-dom";
 
 export default function SignUp() {
-
-    const [name, setName] = useState("")
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    const [confirmPassword, setConfirmPassword] = useState("")
+    const [registrationData, setRegistrationData] = useState({ name: "", email: "", password: "", confirmPassword: "" });
+    const navigate = useNavigate();
 
     function signUp(e) {
         e.preventDefault()
 
-        if (password !== confirmPassword) return alert("As senhas nao sao compativeis")
+        if (registrationData.password !== registrationData.confirmPassword) return alert("As senhas nao sao compativeis")
 
         const url = `${port}/cadastro`
-        const body = { name, email, password }
+        const { confirmPassword, ...register } = registrationData;
+        axios.post(url, register)
+            .then(sucess => {
+                alert(sucess.data);
+                navigate("/login");
+            }).catch(fail => alert(fail.response.data))
+    }
 
-        axios.post(url, body)
-            .then(sucess => alert(sucess.data))
-            .catch(fail => alert(fail.response.data))
+    function insertRegistrationData(event) {
+        event.target.setCustomValidity('');
+        const value = event.target.value;
+        const attribute = event.target.name;
+
+        setRegistrationData({ ...registrationData, [attribute]: value });
     }
 
     return (
@@ -31,10 +37,42 @@ export default function SignUp() {
             <FormContainer>
                 <FormImage src={logo}></FormImage>
                 <PageForm onSubmit={signUp}>
-                    <FormInput placeholder="Nome" type="text" required onChange={(e) => setName(e.target.value)}></FormInput>
-                    <FormInput placeholder="E-mail" type="email" required onChange={(e) => setEmail(e.target.value)}></FormInput>
-                    <FormInput placeholder="Senha" type="password" required onChange={(e) => setPassword(e.target.value)}></FormInput>
-                    <FormInput placeholder="Confirme a senha" type="password" required onChange={(e) => setConfirmPassword(e.target.value)}></FormInput>
+                    <FormInput
+                        placeholder="Nome"
+                        name="name"
+                        value={registrationData.name}
+                        type="text"
+                        required
+                        onChange={insertRegistrationData}
+                        onInvalid={(event) => event.target.setCustomValidity('Por favor, preencha este campo.')}
+                    />
+                    <FormInput
+                        placeholder="E-mail"
+                        type="email"
+                        name="email"
+                        value={registrationData.email}
+                        required
+                        onChange={insertRegistrationData}
+                        onInvalid={(event) => event.target.setCustomValidity('Por favor, insira um email válido.')}
+                    />
+                    <FormInput
+                        placeholder="Senha"
+                        type="password"
+                        name="password"
+                        value={registrationData.password}
+                        required
+                        onChange={insertRegistrationData}
+                        onInvalid={(event) => event.target.setCustomValidity('Por favor, preencha este campo.')}
+                    />
+                    <FormInput
+                        placeholder="Confirme a senha"
+                        type="password"
+                        name="confirmPassword"
+                        value={registrationData.confirmPassword}
+                        required
+                        onChange={insertRegistrationData}
+                        onInvalid={(event) => event.target.setCustomValidity('Por favor, preencha este campo.')}
+                    />
                     <FormButton type="submit">Cadastro</FormButton>
                 </PageForm>
                 <StyledLink to={"/login"}>Ja tem cadastro? Entao siga para o seu login aqui!</StyledLink>
