2f197dc2ec9b9daa0ca5a6a5e67f86d023f6d4cb
winiciuskyutoku <winicius123098@gmail.com>
2023-04-27 21:37:22 -0300

    feat: Header com funcionalidades basicas

---
+++ src/pages/HomePage/style.js
@@ -0,0 +1,12 @@
+import styled from "styled-components"
+
+const MainContainer = styled.div`
+    margin-top: 100px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`
+
+export {
+    MainContainer
+}
